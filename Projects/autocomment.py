d18885e096d19d19b73b96700d228636d3cabbf0
Dimitris Gazetis <d.gazetis@outlook.com>
2021-11-03 23:24:36 +0200

    If conditions for diff times

--- Projects/autocomment.py
+++ Projects/autocomment.py
@@ -1,11 +1,28 @@
 import pyautogui
 import time
 
-comments = "name_1, Name_2, Name_3"
-
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+comments = "@name1 @name @name"
 time.sleep(5)
 
-for i in range(20000):
-    pyautogui.typewrite(comments)
-    pyautogui.click(button='left')
-    time.sleep(50)
+for i in range(200):
+    if i <= 5:
+        pyautogui.typewrite(comments)
+        pyautogui.typewrite("\n")
+        pyautogui.click(button='left')
+        time.sleep(42)
+    elif 5 <= i <= 10:
+        pyautogui.typewrite(comments)
+        pyautogui.typewrite("\n")
+        pyautogui.click(button='left')
+        time.sleep(54)
+    else:
+        pyautogui.typewrite(comments)
+        pyautogui.typewrite("\n")
+        pyautogui.click(button='left')
+        time.sleep(60)
+print(f"Program finished at: {current_time}")
+
+
+
